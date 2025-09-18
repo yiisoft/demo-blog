9a950855f9ecb8ddf7fc5ff7d89d44f89bed2e4e
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-09-18 14:15:44 +0300

    Add "edit category" button

--- src/EndPoint/Web/Blog/Front/Category/template.php
+++ src/EndPoint/Web/Blog/Front/Category/template.php
@@ -6,11 +6,13 @@ use App\Blog\Domain\Category\Category as DomainCategory;
 use App\EndPoint\Web\Blog\Front\Shared\CategoriesPanel\CategoriesPanel;
 use App\EndPoint\Web\Blog\Front\Shared\CategoryReader\Category;
 use App\EndPoint\Web\Blog\Front\Shared\PostsList\PostsList;
+use App\EndPoint\Web\Shared\Access\Permission;
 use App\Shared\Formatter;
 use App\Shared\UrlGenerator;
 use App\EndPoint\Web\Shared\Layout\Breadcrumbs\Breadcrumb;
 use Yiisoft\Data\Paginator\OffsetPaginator;
 use Yiisoft\Html\Html;
+use Yiisoft\Html\NoEncode;
 use Yiisoft\User\CurrentUser;
 use Yiisoft\View\WebView;
 
@@ -36,7 +38,16 @@ $this->addToParameter(
 );
 ?>
 <div class="d-flex justify-content-between align-items-center mb-4">
-    <?= Html::h1($category->name) ?>
+    <h1><?= Html::encode($category->name) ?></h1>
+    <?php if ($currentUser->can(Permission::BlogManage)): ?>
+        <div>
+            <?= Html::a(
+                NoEncode::string('<i class="bi bi-pencil me-1"></i> Edit'),
+                $urlGenerator->categoryUpdate($category->id),
+                ['class' => 'btn btn-outline-primary btn-sm'],
+            ) ?>
+        </div>
+    <?php endif; ?>
 </div>
 <?php if ($category->description !== '') { ?>
     <div class="mb-4">
