21cc6bbefefe2421c9b18627c8cf7c1057973956
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-12-28 12:31:13 +0300

    rbac

--- config/common/di/rbac.php
+++ config/common/di/rbac.php
@@ -14,7 +14,7 @@ return [
         '__construct()' => [
             'itemsStorage' => DynamicReference::to([
                 'class' => ItemsStorage::class,
-                '__construct()' => [dirname(__DIR__, 3) . '/src/Presentation/Site/Shared/Access/config.php'],
+                '__construct()' => [dirname(__DIR__) . '/rbac-items.php'],
             ]),
             'assignmentsStorage' => DynamicReference::to(AssignmentsStorage::class),
         ],
